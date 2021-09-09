9f1ac9f5a01cb75b57261b50c09363a42fedb425
yurim22 <dek1313@naver.com>
2021-09-09 21:00:06 +0900

    chore: 기본 프로젝트 세팅 완료
    eslint 규칙 추가(airbnb 규칙 활용)
    prettier 규칙 추가
    기본 디렉토리 변경(src)

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line arrow-parens
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
