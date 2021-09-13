031317a1bd28e27886a708619f10f98fc3a24691
yurim22 <dek1313@naver.com>
2021-09-13 18:31:04 +0900

    feat: mypage markup 진행 - 미완

---
+++ src/components/MyPage/Profile/Profile.js
@@ -0,0 +1,46 @@
+// // eslint-disable-next-line no-unused-vars
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+    border: 1px solid;
+    width: 20%;
+`;
+const UserInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content:space-around;
+    margin-top: 2rem;
+`;
+const ProfileImage = styled.div`
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+    border: 1px solid;
+`;
+
+const UserName = styled.span`
+    font-weight: bold;
+`;
+
+const UserEmail = styled.div`
+    margin: 2rem 3rem;
+    span{
+        font-weight: bold;
+    }
+`;
+function Profile() {
+  return (
+    <Wrapper>
+      <UserInfo>
+        <ProfileImage />
+        <UserName>서유림</UserName>
+      </UserInfo>
+      <UserEmail><span>이메일</span> : abc@def.com</UserEmail>
+      <UserEmail><span>지갑정보</span> : abc@def.com</UserEmail>
+
+    </Wrapper>
+  );
+}
+
+export default Profile;
--- src/pages/mypage.js
+++ src/pages/mypage.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import Profile from 'components/MyPage/Profile/Profile';
+// import Footer from 'components/Common/Footer';
+import styled from '@emotion/styled';
+import { Switch, Route, Link } from 'react-router-dom';
 
-function mypage() {
-  return <div>마이 페이지</div>;
+const MyPageContainer = styled.div`
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  flex-direction: column;
+`;
+
+const About = () => <div> Youre on the Profile Tab</div>;
+const Comments = () => <div> Youre on the Comments Tab</div>;
+const Contact = () => <div> Youre on the Contact Tab</div>;
+
+function mypage({ match }) {
+  // const About = () => <div> Youre on the Profile Tab</div>;
+  // const Comments = () => <div> Youre on the Comments Tab</div>;
+  // const Contact = () => <div> Youre on the Contact Tab</div>;
+
+  console.log(match);
+  return (
+    <>
+      <MyPageContainer>
+        <h1>마이페이지</h1>
+        <Profile />
+        <div className="links">
+          <Link to={`${match.url}`} className="link">내가 올린 작품</Link>
+          <Link to={`${match.url}/comments`} className="link">내가 구매한 작품</Link>
+          <Link to={`${match.url}/contact`} className="link">좋아요한 작품</Link>
+        </div>
+        <div className="tabs">
+          <Switch>
+            <Route path={`${match.url}`} exact component={About} />
+            <Route path={`${match.url}/comments`} component={Comments} />
+            <Route path={`${match.url}/contact`} component={Contact} />
+          </Switch>
+        </div>
+      </MyPageContainer>
+    </>
+  );
 }
 
 export default mypage;
