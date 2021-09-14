84faf223c234f1f6b356a828ecd511e4ae8b7228
yurim22 <dek1313@naver.com>
2021-09-14 17:06:44 +0900

    feat: 작품 upload button 및 modal container 생성

--- src/components/MyPage/MyUpload/MyUpload.js
+++ src/components/MyPage/MyUpload/MyUpload.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import CardList from '../Card/CardList';
+import Button from './Button';
 
+const MyUploadHeader = styled.div`
+  display :flex;
+  justify-content: space-between;
+`;
 function MyUpload() {
   return (
     <>
-      <div>내가 올린 작품</div>
+      <MyUploadHeader>
+        <div>내가 올린 작품</div>
+        <Button />
+      </MyUploadHeader>
       <CardList />
     </>
   );
