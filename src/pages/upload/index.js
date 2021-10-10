7a13a91b8ecf0ef38d8bf618615535234f3fcdf6
yurim22 <dek1313@naver.com>
2021-10-11 04:19:55 +0900

    [#50] 불필요한 파일 삭제

--- src/pages/upload/index.js
+++ src/pages/upload/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from 'components/Common/Header';
-import UploadForm from 'components/ImgUpload/Form';
+import UploadForm from 'components/ImgUpload';
 import { Title, Container } from './styles';
 
 function Upload() {
